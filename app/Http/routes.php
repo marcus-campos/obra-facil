d835631e99363025fe0daee23b4d58063ee23880
Marcus Campos <marcus_ultimate@hotmail.com>
2016-07-26 19:17:17 -0300

    Estado inicial

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,35 +11,13 @@
 |
 */
 
-Route::get('/', function (){
+/*Route::get('/', function (){
     return view('signin.auth.auth');
-});
+});*/
 
-Route::get('/hash', function () {
-    return Hash::make('123');
-});
-
-Route::post('/signin', ['uses' => 'Auth\AuthController@postLogin', 'as' => 'user.signin']);
-
-Route::group(['before' => 'auth'],function() {
-    Route::group(['prefix' => 'dashboard'], function () {
-        Route::get('home', ['uses' => 'DashBoard\DashBoard\HomeController@show', 'as' => 'dashboard.dashboard.home']);
-    });
-
-    Route::group(['prefix' => 'panel'], function () {
-
-    });
-
-    Route::group(['prefix' => 'finance'], function () {
-
-    });
-
-    Route::group(['prefix' => 'ecommerce'], function () {
-        Route::get('product', ['uses' => 'eCommerce\Products\ProductsController@show', 'as' => 'ecommerce.products.products']);
-    });
+Route::get('/', ['uses' => 'AsoController@show', 'as' => 'dashboard.dashboard.home']);
 
 
-    Route::group(['prefix' => 'settings'], function () {
-        Route::get('user', ['uses' => 'Settings\Users\UserController@show', 'as' => 'settings.users.users']);
-    });
+Route::group(['prefix' => 'panel'], function () {
+    Route::get('/obra', ['uses' => 'DashBoard\DashBoard\HomeController@show', 'as' => 'dashboard.dashboard.home']);
 });
--- resources/views/menu/left-menu.blade.php
+++ resources/views/menu/left-menu.blade.php
@@ -2,37 +2,17 @@
     <div id="sidebar-menu">
         <ul>
             <li class="text-muted menu-title">Painel</li>
-                <li class="has_sub">
-                    <a href="javascript:void(0);" class="waves-effect"><i class="ti-home"></i><span> Dashboard </span> <span class="menu-arrow"></span></a>
-                    <ul class="list-unstyled">
-                        <li><a href="{{ route('dashboard.dashboard.home') }}"> Início </a></li>
-                    </ul>
-                </li>
-            <li class="text-muted menu-title">Administração</li>
 
             <li class="has_sub">
-                <a href="javascript:void(0);" class="waves-effect"><i class="ti-stats-up"></i><span> Financeiro </span> <span class="menu-arrow"></span></a>
-                <ul class="list-unstyled">
-                    <li><a href="#"> Dashboard </a></li>
-                </ul>
+                <a href="#" class="waves-effect"><i class="ti-ruler-pencil"></i><span>Planejando sua obra</span> </a>
             </li>
 
             <li class="has_sub">
-                <a href="javascript:void(0);" class="waves-effect"><i class="ti-shopping-cart"></i><span> eCommerce </span> <span class="menu-arrow"></span></a>
-                <ul class="list-unstyled">
-                    <li><a href="{{ route('ecommerce.products.products') }}"> Cadastro de Produtos</a></li>
-                    <li><a href="#"> Lista de Produtos</a></li>
-                </ul>
+                <a href="#" class="waves-effect"><i class="ti-clipboard"></i><span>Acompanhando a obra</span> </a>
             </li>
 
-            <li class="text-muted menu-title">Configuração</li>
-
             <li class="has_sub">
-                <a href="javascript:void(0);" class="waves-effect"><i class="ti-user"></i><span> Usuários </span> <span class="menu-arrow"></span></a>
-                <ul class="list-unstyled">
-                    <li><a href="{{ route('settings.users.users') }}"> Lista de Usuários</a></li>
-                    <li><a href="#"> Regras de Usuários</a></li>
-                </ul>
+                <a href="#" class="waves-effect"><i class="ti-stats-up"></i><span>Materiais de sua obra</span> </a>
             </li>
 
         </ul>
