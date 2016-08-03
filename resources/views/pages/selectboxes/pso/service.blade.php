d0338eda37dcfc6c786299a69c15e7766e15f372
Marcus Campos <marcus_ultimate@hotmail.com>
2016-08-03 09:35:20 -0300

    Bugfix: Selecione um serviço

--- resources/views/pages/selectboxes/pso/service.blade.php
+++ resources/views/pages/selectboxes/pso/service.blade.php
@@ -1,6 +1,6 @@
 <select class="form-control" id="service" name="service">
-    @foreach($service as $serv)
         <option value="#">Selecione um serviço</option>
+    @foreach($service as $serv)
         <option value="{{ $serv->id }}">{{ $serv->name }}</option>
     @endforeach
 </select>
